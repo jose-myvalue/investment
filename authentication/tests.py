4273bb4368cdb14c0eb0a3b450d67dfa41bea2bd
Adi Chirilov <developer@rosoftware.ro>
2020-05-01 12:35:08 +0300

    Django Dashboard DattaAble PRO - Initial import

---
+++ authentication/tests.py
@@ -0,0 +1,9 @@
+# -*- encoding: utf-8 -*-
+"""
+License: Commercial
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from django.test import TestCase
+
+# Create your tests here.
---
+++ core/urls.py
@@ -0,0 +1,14 @@
+# -*- encoding: utf-8 -*-
+"""
+License: Commercial
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from django.contrib import admin
+from django.urls import path, include  # add this
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("", include("authentication.urls")),  # add this
+    path("", include("app.urls"))  # add this
+]
