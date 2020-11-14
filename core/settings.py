7dc3b8242c09b7be24cf4a7744b00f7178f13bd6
jose-myvalue <jose@myvaluo.io>
2020-11-14 22:40:17 +0000

    server settings

--- core/settings.py
+++ core/settings.py
@@ -20,7 +20,9 @@ SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
 DEBUG = config('DEBUG', default=False)
 
 # load production server from .env
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1')]
+#ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1')]
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1'), '52.59.240.15']
 
 # Application definition
 
